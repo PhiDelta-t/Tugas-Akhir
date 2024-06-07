9f92677e27fd1cf527d0dbc5eed004fb669beb29
Rahmad Abdillah <rahmadabdillah111@gmail.com>
2024-06-07 22:24:35 +0700

    Rahmad : perbaiki crud barang

--- sistem-gudang/resources/views/pages/barang/show.blade.php
+++ sistem-gudang/resources/views/pages/barang/show.blade.php
@@ -1,15 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Detail Barang</h1>
-    <ul>
-        <li><strong>Nama Barang:</strong> {{ $barang->nama_barang }}</li>
-        <li><strong>Deskripsi:</strong> {{ $barang->deskripsi }}</li>
-        <li><strong>Harga:</strong> {{ $barang->harga }}</li>
-        <li><strong>Jumlah Stok:</strong> {{ $barang->jumlah_stok }}</li>
-        <li><strong>Tanggal Produksi:</strong> {{ $barang->tanggal_produksi }}</li>
-        <li><strong>Tanggal Kadaluarsa:</strong> {{ $barang->tanggal_kadaluarsa }}</li>
-        <!-- Tambahkan detail lainnya jika diperlukan -->
-    </ul>
-    <a href="{{ route('barang.index') }}" class="btn btn-primary">Kembali</a>
+    <div class="card">
+        <div class="card-header">
+            Detail Barang
+        </div>
+        <div class="card-body">
+            
+            <p class="card-text"><strong>Nama Barang:</strong> {{ $barang->nama_barang }}</p>
+            <p class="card-text"><strong>Deskripsi:</strong> {{ $barang->deskripsi }}</p>
+            <p class="card-text"><strong>Harga:</strong> {{ $barang->harga }}</p>
+            <p class="card-text"><strong>Jumlah Stok:</strong> {{ $barang->jumlah_stok }}</p>
+            <p class="card-text"><strong>Tanggal Produksi:</strong> {{ $barang->tanggal_produksi }}</p>
+            <p class="card-text"><strong>Tanggal Kadaluarsa:</strong> {{ $barang->tanggal_kadaluarsa }}</p>
+            <p class="card-text"><strong>Nomor Rak:</strong> {{ $barang->rak->no_rak }}</p>
+        </div>
+        <div class="card-footer">
+            <a href="{{ route('barang.index') }}" class="btn btn-primary">Kembali</a>
+        </div>
+    </div>
 @endsection
